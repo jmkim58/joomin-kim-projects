496af3341d5f33191e8633a37f9ed8a9d0e51b40
jmkim58 <43807879+jmkim58@users.noreply.github.com>
2018-10-03 17:39:22 +0900

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -24,58 +24,24 @@ public class Dijkstra {
     // Keep a fast index to nodes in the map
     private Map<String, Vertex> vertexNames;
 
-    /**
-     * Construct an empty Dijkstra with a map. The map's key is the name of a
-     * vertex and the map's value is the vertex object.
-     */
     public Dijkstra() {
 	vertexNames = new HashMap<String, Vertex>();
     }
 
-    /**
-     * Adds a vertex to the dijkstra. Throws IllegalArgumentException if two
-     * vertices with the same name are added.
-     * 
-     * @param v
-     *            (Vertex) vertex to be added to the dijkstra
-     */
     public void addVertex(Vertex v) {
 	if (vertexNames.containsKey(v.name))
 	    throw new IllegalArgumentException("Cannot create new vertex with existing name.");
 	vertexNames.put(v.name, v);
     }
-
-    /**
-     * Gets a collection of all the vertices in the dijkstra
-     * 
-     * @return (Collection<Vertex>) collection of all the vertices in the
-     *         dijkstra
-     */
+	
     public Collection<Vertex> getVertices() {
 	return vertexNames.values();
     }
 
-    /**
-     * Gets the vertex object with the given name
-     * 
-     * @param name
-     *            (String) name of the vertex object requested
-     * @return (Vertex) vertex object associated with the name
-     */
     public Vertex getVertex(String name) {
 	return vertexNames.get(name);
     }
 
-    /**
-     * Adds a directed edge from vertex u to vertex v
-     * 
-     * @param nameU
-     *            (String) name of vertex u
-     * @param nameV
-     *            (String) name of vertex v
-     * @param cost
-     *            (double) cost of the edge between vertex u and v
-     */
     public void addEdge(String nameU, String nameV, Double cost) {
 	if (!vertexNames.containsKey(nameU))
 	    throw new IllegalArgumentException(nameU + " does not exist. Cannot create edge.");
@@ -87,17 +53,6 @@ public class Dijkstra {
 	sourceVertex.addEdge(newEdge);
     }
 
-    /**
-     * Adds an undirected edge between vertex u and vertex v by adding a
-     * directed edge from u to v, then a directed edge from v to u
-     * 
-     * @param nameU
-     *            (String) name of vertex u
-     * @param nameV
-     *            (String) name of vertex v
-     * @param cost
-     *            (double) cost of the edge between vertex u and v
-     */
     public void addUndirectedEdge(String nameU, String nameV, double cost) {
 	addEdge(nameU, nameV, cost);
 	addEdge(nameV, nameU, cost);
@@ -105,28 +60,10 @@ public class Dijkstra {
 
     // MY CODE STARTS HERE
 
-    /**
-     * Computes the euclidean distance between two points as described by their
-     * coordinates
-     * 
-     * @param ux
-     *            (double) x coordinate of point u
-     * @param uy
-     *            (double) y coordinate of point u
-     * @param vx
-     *            (double) x coordinate of point v
-     * @param vy
-     *            (double) y coordinate of point v
-     * @return (double) distance between the two points
-     */
     public double computeEuclideanDistance(double ux, double uy, double vx, double vy) {
 	return Math.sqrt(Math.pow(vx - ux, 2) + Math.pow(vy - uy, 2));
     }
 
-    /**
-     * Calculates the euclidean distance for all edges in the map using the
-     * computeEuclideanCost method.
-     */
     public void computeAllEuclideanDistances() {
 	for (Vertex v : vertexNames.values()) {
 	    for (Edge e : v.adjacentEdges) {
@@ -135,12 +72,6 @@ public class Dijkstra {
 	}
     }
 
-    /**
-     * Dijkstra's Algorithm.
-     * 
-     * @param s
-     *            (String) starting city name
-     */
     public void doDijkstra(String s) {
 	// define infinity
 	final double INFINITY = Double.MAX_VALUE;
@@ -193,11 +124,6 @@ public class Dijkstra {
 
     }
 
-    /**
-     * HElper method that removes vertex with min distance
-     * @param Q the list of vertices
-     * @return the removed vertex
-     */
     private Vertex removeMin(ArrayList<Vertex> Q) {
 	Vertex min = Q.get(0);
 	for (int i = 1; i < Q.size(); i++) {
@@ -211,16 +137,6 @@ public class Dijkstra {
 	return min;
     }
 
-    /**
-     * Returns a list of edges for a path from city s to city t. This will be
-     * the shortest path from s to t as prescribed by Dijkstra's algorithm
-     * 
-     * @param s
-     *            (String) starting city name
-     * @param t
-     *            (String) ending city name
-     * @return (List<Edge>) list of edges from s to t
-     */
     public List<Edge> getDijkstraPath(String s, String t) {
 	doDijkstra(s);
 
@@ -239,7 +155,6 @@ public class Dijkstra {
 	}
 	list.add(0, u);
 	
-	
 	// find edges from sequence of vertices
 	List<Edge> path = new ArrayList<>();
 	for (int i = 1; i < list.size(); i++) {
@@ -256,7 +171,7 @@ public class Dijkstra {
 	
     }
 
-    // STUDENT CODE ENDS HERE
+    // MY CODE ENDS HERE
 
     /**
      * Prints out the adjacency list of the dijkstra for debugging
@@ -277,11 +192,6 @@ public class Dijkstra {
 	}
     }
 
-    /**
-     * A main method that illustrates how the GUI uses Dijkstra.java to read a
-     * map and represent it as a graph. You can modify this method to test your
-     * code on the command line.
-     */
     public static void main(String[] argv) throws IOException {
 	String vertexFile = "cityxy.txt";
 	String edgeFile = "citypairs.txt";
@@ -316,22 +226,13 @@ public class Dijkstra {
 	}
 	edgeFileBr.close();
 
-	// Compute distances.
-	// This is what happens when you click on the "Compute All Euclidean
-	// Distances" button.
 	dijkstra.computeAllEuclideanDistances();
 
-	// print out an adjacency list representation of the graph
 	dijkstra.printAdjacencyList();
 
-	// This is what happens when you click on the "Draw Dijkstra's Path"
-	// button.
-
-	// In the GUI, these are set through the drop-down menus.
 	String startCity = "SanFrancisco";
 	String endCity = "Boston";
 
-	// Get weighted shortest path between start and end city.
 	List<Edge> path = dijkstra.getDijkstraPath(startCity, endCity);
 
 	System.out.print("Shortest path between " + startCity + " and " + endCity + ": ");
