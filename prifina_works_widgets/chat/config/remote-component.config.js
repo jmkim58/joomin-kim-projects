dc41339f3d829a651dc13d329d802722198b5ddd
jmkim58 <43807879+jmkim58@users.noreply.github.com>
2021-04-22 14:47:53 -0400

    Add files via upload

---
+++ prifina_works_widgets/chat/config/remote-component.config.js
@@ -0,0 +1,13 @@
+/**
+ * Dependencies for Remote Components
+ *
+ */
+
+module.exports = {
+  resolve: {
+    react: require("react"),
+    "styled-components": require("styled-components"),
+    "@prifina/hooks": require("@prifina/hooks"),
+    "@paciolan/remote-component": "^2.11.0",
+  },
+};
